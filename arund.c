a8c80523b80d3337837d4944eb8310cf6cf1a181
arunperukudi <arunperukudi@gmail.com>
2017-07-21 17:53:23 +0530

    Create arund.c

---
+++ arund.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main()
+{
+int a=0;
+printf("enter the value of a");
+scanf("%d",&a);
+if(a>=0)
+{
+printf("a is positive number");
+else
+{
+printf("a is negativenumber");
+}
+}
+}
